45b53e643f042de60cf09d1836f66e3f3cb41c54
Maxwell Griffith <maxwellgriffith345@gmail.com>
2026-05-04 08:14:26 -0500

    more basic aggs

--- queries/basic_aggs.sql
+++ queries/basic_aggs.sql
@@ -1,9 +1,8 @@
 -- Basic Aggregations
 
-
 -- Query1: Average electricity prices by state
 -- across the enitre date range and all sectors
-SELECT ds.state_short, AVG(fp.price_per_kwh) as avg_price
+SELECT ds.state_short, ROUND(AVG(fp.price_per_kwh),2) as avg_price
 FROM fact_prices fp
 JOIN dim_state ds ON fp.state_id = ds.state_id
 GROUP BY ds.state_short
@@ -15,4 +14,24 @@ select df.fuel_long, SUM(fg.generation ) as total_gen
 from fact_gen fg 
 join dim_fuel df on fg.fuel_id  = df.fuel_id 
 group by df.fuel_long
-order by total_gen DESC
+order by total_gen desc
+
+-- Query 3: average price by state and sector
+SELECT ds.state_short, dp.sector_name, ROUND(AVG(fp.price_per_kwh),2) as avg_price
+FROM fact_prices fp
+JOIN dim_state ds ON fp.state_id = ds.state_id
+join dim_pricesector dp on fp.sector_id =dp.sector_id
+WHERE fp.price_per_kwh  is not NULL
+GROUP BY ds.state_short, dp.sector_name
+ORDER BY avg_price desc
+
+-- Query 4: Top 5 states by NG price
+select ds.state_short , ROUND(avg(ffp.price_per_mmbtu ),2) as avgNG_price
+from fact_fuel_prices ffp
+join dim_state ds on ffp.state_id = ds.state_id 
+join dim_fuel df on ffp.fuel_id = df.fuel_id 
+where df.fuel_short = 'NG'
+group by ds.state_short 
+limit 5
+
+
