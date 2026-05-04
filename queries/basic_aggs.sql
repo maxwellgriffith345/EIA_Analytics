17828570e7deeab08f29059739272fd76af02ec6
Maxwell Griffith <maxwellgriffith345@gmail.com>
2026-05-04 18:31:41 -0500

    started ctes

--- queries/basic_aggs.sql
+++ queries/basic_aggs.sql
@@ -34,4 +34,26 @@ where df.fuel_short = 'NG'
 group by ds.state_short 
 limit 5
 
+-- Query 5 — States with consistently high prices
+SELECT ds.state_short, ROUND(AVG(fp.price_per_kwh),2) as avg_price
+FROM fact_prices fp
+JOIN dim_state ds ON fp.state_id = ds.state_id
+GROUP BY ds.state_short
+having AVG(fp.price_per_kwh )> 15
+ORDER BY avg_price DESC
+
+-- Query 6 — Generation mix for California (id = 5)
+-- what percentage of total generation comes from each fuel type
+select ds.state_short, df.fuel_long, SUM(fg.generation ) as total_gen, 
+	Round((SUM(fg.generation)*100/(select SUM(fg.generation) 
+	from fact_gen fg 
+	where fg.state_id = 5)),2) as percent_gen
+from fact_gen fg
+join dim_state ds  on fg.state_id = ds.state_id 
+join dim_fuel df  on fg.fuel_id = df.fuel_id 
+where ds.state_short = 'CA'
+group by ds.state_short, df.fuel_long
+
+
+
 
--- queries/ctes.sql
+++ queries/ctes.sql
@@ -1,1 +1,53 @@
 -- This is where the ctes go
+
+-- Query 7
+-- Average price by state, ranked for states above national average
+with cte_avg_price as (
+  	SELECT ds.state_short, ROUND(AVG(fp.price_per_kwh), 2) as avg_price
+    FROM fact_prices fp
+    JOIN dim_state ds ON fp.state_id = ds.state_id
+    GROUP BY ds.state_short
+)
+
+select * 
+from cte_avg_price
+where avg_price > (select avg(fp.price_per_kwh) from fact_prices fp)
+order by avg_price desc
+
+-- Query 8: FILTER OUT "ALL FUELS"
+-- Generation mix percentage by state
+--cte generation per state per fuel type
+with cte_gen_state_fuel as(
+	select ds.state_short, df.fuel_long, SUM(fg.generation ) as fuel_gen 
+	from fact_gen fg
+	join dim_state ds  on fg.state_id = ds.state_id 
+	join dim_fuel df  on fg.fuel_id = df.fuel_id 
+	group by ds.state_short, df.fuel_long
+),
+cte_total_gen_state as(
+	select ds.state_short, SUM(fg.generation) as state_gen
+	from fact_gen fg 
+	join dim_state ds on fg.state_id = ds.state_id
+	group by ds.state_short 
+)
+
+select gst.state_short, gst.fuel_long, gst.fuel_gen, gs.state_gen,
+	Round((gst.fuel_gen *100/gs.state_gen),2) as percentage_gen
+from cte_gen_state_fuel gst
+join cte_total_gen_state gs on gst.state_short = gs.state_short
+order by gst.state_short;
+
+
+-- Query 9 TODO — Month with peak electricity prices per state
+
+-- CTE average price by state and month
+select 
+
+
+SELECT ds.state_short, fp.date_id, ROUND(AVG(fp.price_per_kwh),2) as avg_price
+FROM fact_prices fp
+JOIN dim_state ds ON fp.state_id = ds.state_id
+GROUP BY ds.state_short, fp.date_id
+ORDER BY ds.state_short DESC
+
+
